70aca298d32dc4fd8083afcb7afa3581343a7b9a
Wahyu Pramudya <wartegboy098@gmail.com>
2023-11-14 23:39:25 +0700

    widget/button_method : add button method widget

---
+++ lib/view/widget/button_method.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:toggle_switch/toggle_switch.dart';
+
+class ButtonMethod extends StatefulWidget {
+  const ButtonMethod({super.key});
+
+  @override
+  State<ButtonMethod> createState() => _ButtonMethodState();
+}
+
+class _ButtonMethodState extends State<ButtonMethod> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Container(
+        height: 60,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12.0),
+          color: const Color(0xFFCCE7FF),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(6.0),
+          child: ToggleSwitch(
+            minWidth: 200,
+            minHeight: 50,
+            cornerRadius: 8,
+            fontSize: 14,
+            totalSwitches: 2,
+            radiusStyle: true,
+            activeBgColors: const [[Color.fromARGB(255, 0, 133, 255)],[Color.fromARGB(255, 0, 133, 255)]],
+            inactiveBgColor:const Color(0xFFCCE7FF),
+            labels: const ["E-Wallet","Bank-Transfer"],
+            inactiveFgColor: Colors.black,
+            onToggle: (index){
+              // if(index == 0){
+              //   _showDialog(context);
+              // }else{
+              //   _ratingDialog(context);
+              // }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
