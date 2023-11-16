6c0cbc19c98d1845d41270bd66fdf8fcf107947f
itsjipa <nazhiifahn@gmail.com>
2023-11-16 14:59:52 +0700

    widget/profile_psikolog : add widget for profile psikolog

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:empathi_care/view/widget/upload_image_widget.dart';
+import 'package:empathi_care/view/widget/profile_psikolog_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const UploadImageWidget(),
+      home: const ProfilePsikologWidget(),
     );
   }
 }
