56410b808d421c3f4d37f4dda4286564e006c4c3
Denarizki <86529218+DenaRizki1@users.noreply.github.com>
2023-11-18 20:51:37 +0700

    widget/card_paket: perubahan pada widget card paket

--- lib/view/widget/card_paket_widget.dart
+++ lib/view/widget/card_paket_widget.dart
@@ -1,76 +1,71 @@
 import 'package:flutter/material.dart';
 
-class CardPaketWidget extends StatefulWidget {
-  const CardPaketWidget({super.key});
-
-  @override
-  State<CardPaketWidget> createState() => _CardPaketWidgetState();
-}
-
-class _CardPaketWidgetState extends State<CardPaketWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.zero,
-      elevation: 4,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-      color: Colors.white,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            padding: const EdgeInsets.all(10),
-            decoration: const BoxDecoration(color: Color(0xffCCE7FF), borderRadius: BorderRadius.only(topLeft: Radius.circular(8), topRight: Radius.circular(9))),
-            child: Row(
-              // crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Container(
-                  height: 60,
-                  width: 60,
-                  decoration: const BoxDecoration(
-                    color: Colors.grey,
-                    shape: BoxShape.circle,
-                    // image: DecorationImage(image: AssetImage()),
+Widget CardPaketWidget({required String title, required String price, required List profit}) {
+  return Card(
+    margin: EdgeInsets.zero,
+    elevation: 4,
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+    color: Colors.white,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(10),
+          decoration: const BoxDecoration(color: Color(0xffCCE7FF), borderRadius: BorderRadius.only(topLeft: Radius.circular(8), topRight: Radius.circular(9))),
+          child: Row(
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                height: 60,
+                width: 60,
+                decoration: const BoxDecoration(
+                  // color: Colors.grey,
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage("assets/icons/icons_paket.png"),
+                    fit: BoxFit.fill,
                   ),
                 ),
-                const SizedBox(width: 12),
-                const Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Paket Perkenalan",
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    Text(
-                      "Rp. 80.000/1 Sesi",
-                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                )
-              ],
-            ),
+              ),
+              const SizedBox(width: 12),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    price,
+                    style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              )
+            ],
           ),
-          const Padding(
-            padding: EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Metode Chat",
-                  style: TextStyle(fontSize: 14),
-                ),
-                SizedBox(height: 4),
-                Text(
-                  "Durasi 30 Menit",
-                  style: TextStyle(fontSize: 14),
-                ),
-              ],
-            ),
-          )
-        ],
-      ),
-    );
-  }
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: profit.length,
+                itemBuilder: (context, index) {
+                  return Text(
+                    "\u2022 ${profit[index].toString()}",
+                    style: const TextStyle(fontSize: 14),
+                  );
+                },
+              )
+            ],
+          ),
+        )
+      ],
+    ),
+  );
 }
