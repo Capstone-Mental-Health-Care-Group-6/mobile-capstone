e0835a820176f64d19ba7b8e90145dbd6ecbca57
Abdulloh Fahmi <abdullohfahmi45@gmail.com>
2023-11-21 23:02:48 +0700

    widget/card_psikolog_on_payment_method: update image leading

--- lib/view/widget/card_psikolog_on_payment_method.dart
+++ lib/view/widget/card_psikolog_on_payment_method.dart
@@ -19,12 +19,19 @@ class _CardPsikologOnPaymentMethodState
           color: Color(0xFFCCE7FF),
           borderRadius: BorderRadius.all(Radius.circular(10))),
       child: ListTile(
-        leading: CircleAvatar(
-            child: Image.asset(
-          'assets/image/logo-psikolog.png',
-        )),
+        leading: Container(
+            height: 50,
+            width: 50,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage(
+                    'assets/image/logo-psikolog.png',
+                  )),
+            )),
         title: Text('Rangga S.Psi., M.Psi',
-            style: GoogleFonts.montserrat(fontWeight: FontWeight.w600)),
+            style: GoogleFonts.montserrat(fontWeight: FontWeight.w700)),
         subtitle: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
