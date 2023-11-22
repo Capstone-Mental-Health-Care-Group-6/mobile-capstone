5bfff62190d8edd24f8e72276869aa670d384a8a
Dhea <107859825+aadeeee@users.noreply.github.com>
2023-11-22 21:35:00 +0700

    Update date.dart

--- lib/utils/constant/date.dart
+++ lib/utils/constant/date.dart
@@ -1,8 +1,5 @@
 import 'package:intl/intl.dart';
 
-String get formattedDate => DateFormat('EEEE', 'id').format(DateTime.now());
-
-String get formattedHour => DateFormat('HH.mm').format(DateTime.now());
 String getFormattedDateRiwayat(DateTime specificDate) {
   return DateFormat('dd MMM, HH:mm', 'id').format(specificDate);
 }
