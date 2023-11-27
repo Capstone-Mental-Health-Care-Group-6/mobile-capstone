7a02764fcf32fb3d531ca9e0b9d4de32660cca68
Jeriko <Jeriko8304@gmail.com>
2023-11-27 20:47:56 +0700

    widget/add delete Logout widget

---
+++ lib/view/widget/DeleteLogOutProfile/logout_widget.dart
@@ -0,0 +1,60 @@
+import 'package:empathi_care/view/screen/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+logOutWidget(BuildContext context) {
+  return showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(15))),
+        backgroundColor: const Color(0xffffffff),
+        title: Text(
+          'Apakah kamu yakin ingin\nkeluar?',
+          textAlign: TextAlign.center,
+          style:
+              GoogleFonts.montserrat(fontSize: 14, fontWeight: FontWeight.w700),
+        ),
+        content: SizedBox(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(120, 48),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    backgroundColor: const Color(0XFF0085FF),
+                    foregroundColor: const Color.fromRGBO(255, 255, 255, 1)),
+                child: const Text('Batal'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 16),
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    side: const BorderSide(color: Color(0XFF0085FF)),
+                    minimumSize: const Size(120, 48),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    backgroundColor: Colors.transparent,
+                    foregroundColor: const Color(0XFF0085FF),
+                  ),
+                  child: const Text('Keluar'),
+                  onPressed: () {
+                    Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (_) => const LoginScreen()));
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}
