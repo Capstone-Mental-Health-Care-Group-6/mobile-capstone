6d0ee18758d43f05bbe2aeef827f21fcdbbddbce
Jeriko <Jeriko8304@gmail.com>
2023-11-27 19:36:05 +0700

    info/add ui, aboutus, privacy, and edit terms

--- lib/view/screen/Register/terms_screen.dart
+++ lib/view/screen/Register/terms_screen.dart
@@ -1,4 +1,3 @@
-import 'package:empathi_care/view/widget/syarat_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -7,66 +6,26 @@ class TermsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<String> syarat = [
-      'Penggunaan Informasi: Kami menyediakan layanan konsultasi dengan psikolog melalui chat, video call, dan zoom call. Informasi yang Anda berikan selama konsultasi akan dianggap rahasia, kecuali dalam kasus yang diwajibkan oleh hukum atau kebijakan perusahaan kami.',
-      'Batasan Layanan: Layanan konsultasi dengan psikolog adalah sarana pendukung dan bukan pengganti konsultasi medis langsung. Kami tidak bertanggung jawab atas hasil dari konsultasi yang dilakukan.',
-      'Ketersediaan Layanan: Layanan kami mungkin tidak selalu tersedia 24/7. Kami akan berusaha sebaik mungkin untuk memberikan layanan secepat mungkin, tetapi kami tidak menjamin ketersediaan konsultasi dalam waktu tertentu.',
-      'Pendaftaran: Anda bertanggung jawab untuk memberikan informasi pendaftaran yang akurat dan terbaru. Anda tidak boleh memberikan informasi palsu atau mengambil identitas orang lain.',
-      'Tindakan Pengguna: Anda setuju untuk tidak menggunakan aplikasi ini untuk tujuan ilegal atau melanggar hukum.',
-      'Konten Aplikasi: Aplikasi ini dapat berisi artikel kesehatan mental yang bertujuan untuk memberikan informasi umum. Artikel ini tidak menggantikan saran medis atau konsultasi dengan seorang profesional kesehatan mental.',
-      'Privasi: Kami menghormati privasi Anda. Informasi pribadi yang Anda berikan akan dikelola sesuai dengan kebijakan privasi kami.',
-      'Keamanan Akun: Anda bertanggung jawab atas keamanan akun Anda. Jangan memberikan informasi login Anda kepada orang lain.',
-      'Pembaruan Syarat dan Ketentuan: Kami berhak untuk memperbarui atau mengubah syarat dan ketentuan ini dari waktu ke waktu. Perubahan tersebut akan diberitahukan kepada Anda melalui aplikasi atau email.',
-      'Pengakhiran Akun: Kami berhak untuk mengakhiri akun pengguna tanpa pemberitahuan jika Anda melanggar syarat dan ketentuan ini.'
-    ];
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false,
-        actions: [
-          IconButton(
-            icon: const Icon(
-              Icons.close,
-              size: 30,
-            ),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          )
-        ],
+        title: Text(
+          'Syarat & Ketentuan',
+          style:
+              GoogleFonts.montserrat(fontSize: 16, fontWeight: FontWeight.w700),
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(right: 10, left: 10),
-        child: ListView(
-          children: [
-            Text(
-              'Syarat & Ketentuan',
-              style: GoogleFonts.montserrat(
-                  fontSize: 24, fontWeight: FontWeight.w600),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: Text(
-                '\nDengan mengakses dan menggunakan aplikasi ini, Anda setuju untuk terikat oleh syarat dan ketentuan ini. Jika Anda tidak setuju dengan syarat dan ketentuan ini, mohon untuk tidak menggunakan aplikasi kami.\n',
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(right: 20, left: 20, bottom: 35),
+          child: Column(
+            children: [
+              Text(
+                'Selamat datang di EmpathiCare.\n\nSebelum Anda melanjutkan untuk menggunakan layanan kami, kami sangat menyarankan Anda untuk membaca dan memahami syarat dan ketentuan berikut ini. Dengan mengakses dan menggunakan aplikasi ini, Anda setuju untuk terikat oleh syarat dan ketentuan ini. Jika Anda tidak setuju dengan syarat dan ketentuan ini, mohon untuk tidak menggunakan aplikasi kami.\n\nPenggunaan Informasi: Kami menyediakan layanan konsultasi dengan psikolog melalui chat, video call, dan zoom call. Informasi yang Anda berikan selama konsultasi akan dianggap rahasia, kecuali dalam kasus yang diwajibkan oleh hukum atau kebijakan perusahaan kami.\nBatasan Layanan: Layanan konsultasi dengan psikolog adalah sarana pendukung dan bukan pengganti konsultasi medis langsung. Kami tidak bertanggung jawab atas hasil dari konsultasi yang dilakukan.\nKetersediaan Layanan: Layanan kami mungkin tidak selalu tersedia 24/7. Kami akan berusaha sebaik mungkin untuk memberikan layanan secepat mungkin, tetapi kami tidak menjamin ketersediaan konsultasi dalam waktu tertentu.\nPendaftaran: Anda bertanggung jawab untuk memberikan informasi pendaftaran yang akurat dan terbaru. Anda tidak boleh memberikan informasi palsu atau mengambil identitas orang lain.\nTindakan Pengguna: Anda setuju untuk tidak menggunakan aplikasi ini untuk tujuan ilegal atau melanggar hukum.\nKonten Aplikasi: Aplikasi ini dapat berisi artikel kesehatan mental yang bertujuan untuk memberikan informasi umum. Artikel ini tidak menggantikan saran medis atau konsultasi dengan seorang profesional kesehatan mental.\nPrivasi: Kami menghormati privasi Anda. Informasi pribadi yang Anda berikan akan dikelola sesuai dengan kebijakan privasi kami.\nKeamanan Akun: Anda bertanggung jawab atas keamanan akun Anda. Jangan memberikan informasi login Anda kepada orang lain.\nPembaruan Syarat dan Ketentuan: Kami berhak untuk memperbaiki atau mengubah syarat dan ketentuan ini dari waktu ke waktu. Perubahan tersebut akan diberitahukan kepada Anda melalui aplikasi atau email.\nPengakhiran Akun: Kami berhak untuk mengakhiri akun pengguna tanpa pemberitahuan jika Anda melanggar syarat dan ketentuan ini.\n\nDengan menggunakan aplikasi ini, Anda setuju untuk mematuhi syarat dan ketentuan ini. Mohon diingat bahwa ini hanyalah contoh, dan Anda sebaiknya berkonsultasi dengan seorang profesional hukum untuk memastikan bahwa syarat dan ketentuan tersebut sesuai dengan hukum yang berlaku di wilayah Anda.',
                 style: GoogleFonts.montserrat(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 18,
-                    height: 1.2,
-                    color: const Color(0xff070707)),
+                    fontSize: 12, fontWeight: FontWeight.w500),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: NumberedText(items: syarat),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10, bottom: 50),
-              child: Text(
-                '\nDengan menggunakan aplikasi ini, Anda setuju untuk mematuhi syarat dan ketentuan ini. Mohon diingat bahwa ini hanyalah contoh, dan Anda sebaiknya berkonsultasi dengan seorang profesional hukum untuk memastikan bahwa syarat dan ketentuan tersebut sesuai dengan hukum yang berlaku di wilayah Anda.',
-                style: GoogleFonts.montserrat(
-                    fontSize: 18, fontWeight: FontWeight.w500, height: 1.2),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
