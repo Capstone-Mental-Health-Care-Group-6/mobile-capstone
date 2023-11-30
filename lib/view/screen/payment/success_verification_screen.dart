3e22d7b66cf0353636698826df2755dddef487f7
Denarizki <86529218+DenaRizki1@users.noreply.github.com>
2023-11-30 19:49:17 +0700

    screen/pembayaran_manual_screen: update ui pembayaran manual

---
+++ lib/view/screen/payment/success_verification_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SuccessVerificationScreen extends StatefulWidget {
+  const SuccessVerificationScreen({super.key});
+
+  @override
+  State<SuccessVerificationScreen> createState() => _SuccessVerificationScreenState();
+}
+
+class _SuccessVerificationScreenState extends State<SuccessVerificationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              width: double.infinity,
+              height: 190,
+              child: Image(
+                image: AssetImage("assets/images/success.png"),
+              ),
+            ),
+            const SizedBox(height: 24),
+            Text(
+              "Yeay, Pembayaran Berhasil",
+              style: GoogleFonts.montserrat(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 24),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Text(
+                  "Mulai Konseling",
+                  style: GoogleFonts.montserrat(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            const SizedBox(height: 8),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    side: const BorderSide(color: Colors.blue),
+                  ),
+                ),
+                child: Text(
+                  "Kembali ke home",
+                  style: GoogleFonts.montserrat(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
