fc0caf533bc4498e657171a5725581acfe40d02a
Denarizki <86529218+DenaRizki1@users.noreply.github.com>
2023-12-15 03:54:17 +0700

    screen/pembayaran_manual_screen: implementasi api ,view model dan service

--- lib/view/screen/payment/pembayaran_manual_screen.dart
+++ lib/view/screen/payment/pembayaran_manual_screen.dart
@@ -1,10 +1,13 @@
+import 'dart:io';
+
 import 'package:dotted_border/dotted_border.dart';
-import 'package:empathi_care/view/screen/payment/verification_payment_screen.dart';
 import 'package:empathi_care/view/widget/count_down_payment_widget.dart';
 import 'package:empathi_care/view/widget/payment_instruction_list.dart';
+import 'package:empathi_care/view_model/pembayaran_manual_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 class PembayaranManualScreen extends StatefulWidget {
   const PembayaranManualScreen({super.key});
@@ -14,8 +17,18 @@ class PembayaranManualScreen extends StatefulWidget {
 }
 
 class _PembayaranManualScreenState extends State<PembayaranManualScreen> {
+  late PembayaranManualProvider prov;
+  @override
+  void initState() {
+    prov = context.read<PembayaranManualProvider>();
+
+    prov.init();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final pembayaranProv = Provider.of<PembayaranManualProvider>(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -169,88 +182,129 @@ class _PembayaranManualScreenState extends State<PembayaranManualScreen> {
                   ),
                 ),
                 const SizedBox(height: 16),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                  width: double.infinity,
-                  decoration: const BoxDecoration(
-                    color: Color.fromRGBO(242, 242, 242, 1.0),
-                  ),
-                  alignment: Alignment.center,
-                  child: DottedBorder(
-                    borderType: BorderType.RRect,
-                    radius: const Radius.circular(
-                      8.0,
-                    ),
-                    dashPattern: const [4.0, 4.0],
-                    child: SizedBox(
-                      width: 328.0,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            SvgPicture.asset(
-                              'assets/icons/download.svg',
-                              width: 34.0,
-                              height: 34.0,
-                            ),
-                            const SizedBox(
-                              height: 12.0,
-                            ),
-                            Text(
-                              'or',
-                              style: GoogleFonts.roboto(
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.w400,
-                                color: Colors.black,
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 12.0,
-                            ),
-                            SizedBox(
-                              width: 120.0,
-                              height: 40.0,
-                              child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(
-                                      8.0,
+                prov.fileImage == ""
+                    ? Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                        width: double.infinity,
+                        decoration: const BoxDecoration(
+                          color: Color.fromRGBO(242, 242, 242, 1.0),
+                        ),
+                        alignment: Alignment.center,
+                        child: DottedBorder(
+                          borderType: BorderType.RRect,
+                          radius: const Radius.circular(
+                            8.0,
+                          ),
+                          dashPattern: const [4.0, 4.0],
+                          child: SizedBox(
+                            width: 328.0,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  SvgPicture.asset(
+                                    'assets/icons/download.svg',
+                                    width: 34.0,
+                                    height: 34.0,
+                                  ),
+                                  const SizedBox(
+                                    height: 12.0,
+                                  ),
+                                  Text(
+                                    'or',
+                                    style: GoogleFonts.roboto(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.w400,
+                                      color: Colors.black,
                                     ),
                                   ),
-                                  backgroundColor: const Color(0xff0085FF),
-                                  foregroundColor: Colors.white,
+                                  const SizedBox(
+                                    height: 12.0,
+                                  ),
+                                  SizedBox(
+                                    width: 120.0,
+                                    height: 40.0,
+                                    child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(
+                                            8.0,
+                                          ),
+                                        ),
+                                        backgroundColor: const Color(0xff0085FF),
+                                        foregroundColor: Colors.white,
+                                      ),
+                                      onPressed: () {
+                                        pembayaranProv.showDialogImagePicker(context);
+                                      },
+                                      child: Text(
+                                        'Select file',
+                                        style: GoogleFonts.montserrat(
+                                          fontSize: 12.0,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 10.0,
+                                  ),
+                                  Text(
+                                    textAlign: TextAlign.center,
+                                    'Recommended Image Size 300x450. Only Accept: jpg,png.jpeg.',
+                                    style: GoogleFonts.montserrat(
+                                      fontSize: 10.0,
+                                      fontWeight: FontWeight.w500,
+                                      color: const Color(0xff666666),
+                                      decoration: TextDecoration.none,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    : Stack(
+                        children: [
+                          SizedBox(
+                            height: 250,
+                            width: double.infinity,
+                            child: Image(
+                              image: FileImage(
+                                File(prov.fileImage),
+                              ),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.topRight,
+                            child: InkWell(
+                              onTap: () {
+                                prov.onTapRemove();
+                              },
+                              child: Container(
+                                margin: const EdgeInsets.only(top: 6, right: 6),
+                                height: 20,
+                                width: 20,
+                                decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.circular(4),
                                 ),
-                                onPressed: () {},
-                                child: Text(
-                                  'Select file',
-                                  style: GoogleFonts.montserrat(
-                                    fontSize: 12.0,
-                                    fontWeight: FontWeight.w600,
+                                child: const Center(
+                                  child: Icon(
+                                    Icons.close,
+                                    color: Colors.white,
+                                    size: 16,
                                   ),
                                 ),
                               ),
                             ),
-                            const SizedBox(
-                              height: 10.0,
-                            ),
-                            Text(
-                              textAlign: TextAlign.center,
-                              'Recommended Image Size 300x450. Only Accept: jpg,png.jpeg.',
-                              style: GoogleFonts.montserrat(
-                                fontSize: 10.0,
-                                fontWeight: FontWeight.w500,
-                                color: const Color(0xff666666),
-                                decoration: TextDecoration.none,
-                              ),
-                            ),
-                          ],
-                        ),
+                          )
+                        ],
                       ),
-                    ),
-                  ),
-                ),
                 const SizedBox(height: 16),
                 Text(
                   "Upload Bukti Pembayaran",
@@ -270,10 +324,14 @@ class _PembayaranManualScreenState extends State<PembayaranManualScreen> {
                 SizedBox(
                   width: double.infinity,
                   child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => const VerificationPaymentScreen(),
-                        ));
+                      onPressed: () async {
+                        final result = await prov.addTransaction();
+
+                        if (result) {
+                          // Navigator.of(context).push(MaterialPageRoute(
+                          //   builder: (context) => const VerificationPaymentScreen(),
+                          // ));
+                        }
                       },
                       style: ElevatedButton.styleFrom(
                         shape: RoundedRectangleBorder(
